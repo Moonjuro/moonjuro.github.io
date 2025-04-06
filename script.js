4b0e93b8557470f24e8e6f0f7fc63254a6a2d94a
Moonjuro <168666057+Moonjuro@users.noreply.github.com>
2025-04-06 17:33:41 +0200

    added a comment for initial count
    yea useless i know

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const list = document.getElementById('todo-list');
 const itemCountSpan = document.getElementById('item-count');
 const uncheckedCountSpan = document.getElementById('unchecked-count');
 
+// Initial count
 let count = 3;
 itemCountSpan.innerText = count;
 
